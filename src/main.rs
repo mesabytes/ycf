6fcb8045fd3eca836cb6d9619d59d716cc2160a3
abdalrzag eisa <bmjfdrh@gmail.com>
2022-06-19 23:21:54 +0300

    fix: missing )

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use neoconf::neoconf::Neoconf;
 
 fn main() {
-    let config = Neoconf::load(String::from("test.neoconf");
+    let config = Neoconf::load(String::from("test.neoconf"));
 
     println!("hi");
 }
