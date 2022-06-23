b2e0d03336eb28881d6a1a7be1a9410e2be5261c
abdalrzag eisa <bmjfdrh@gmail.com>
2022-06-23 22:22:26 +0300

    install-syntax-highlighting.sh

---
+++ install-syntax-highlighting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install the syntax highlighting vscode extension
+
+VSCODE_EXTENSION_PATH=${HOME}/.vscode-oss/extensions
+
+cp -rv ./neoconf-syntax-highlighting ${VSCODE_EXTENSION_PATH}
