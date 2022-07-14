c8eb85f503ff595e4b3046b0a424f919e423443c
abdalrzag eisa <bmjfdrh@gmail.com>
2022-07-14 21:17:18 +0300

    add save method

--- src/lib.rs
+++ src/lib.rs
@@ -13,25 +13,26 @@ impl Neoconf {
         Self { file_path, storage: HashMap::new() }
     }
 
-    fn get_section_name(&self, section: Option<&str>) -> String {
-        match section {
-            Some(x) => {
-                return x.to_string()
-            },
-            None => {
-                return DEFAULT_SECTION.to_string()
-            }
-        }
+    pub fn load(&mut self) {
+        let file_contents = self.get_file_contents();
+        let mut parser = Parser::new(self.file_path.to_string(), file_contents);
+
+        self.storage = parser.parse();
+        // self.parse(file_contents);
+    }
+
+    pub fn save(&self) {
+        
     }
 
     pub fn get(&self, section: Option<&str>, key: &str) -> Option<&String> {
-        let section_name = self.get_section_name(section);
+        let section_name = get_section_name(section);
 
         return self.storage.get(&format!("{section_name}.{key}"))
     }
 
     pub fn set(&mut self, section: Option<&str>, key: &str, value: &str) {
-        let section_name = self.get_section_name(section);
+        let section_name = get_section_name(section);
 
         let key = format!("{section_name}.{key}");
 
@@ -39,19 +40,11 @@ impl Neoconf {
     }
 
     pub fn remove(&mut self, section: Option<&str>, key: &str) {
-        let section_name = self.get_section_name(section);
+        let section_name = get_section_name(section);
 
         let key = format!("{section_name}.{key}");
 
         self.storage.remove(&key);
-    }
-
-    pub fn load(&mut self) {
-        let file_contents = self.get_file_contents();
-        let mut parser = Parser::new(self.file_path.to_string(), file_contents);
-
-        self.storage = parser.parse();
-        // self.parse(file_contents);
     }
 
     /// read config file and return file contents
@@ -63,6 +56,17 @@ impl Neoconf {
         let contents = std::fs::read_to_string(&self.file_path).expect("Failed to read file");
 
         return contents;
+    }
+}
+
+fn get_section_name(section: Option<&str>) -> String {
+    match section {
+        Some(x) => {
+            return x.to_string()
+        },
+        None => {
+            return DEFAULT_SECTION.to_string()
+        }
     }
 }
 
