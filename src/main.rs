986b543cdf31eb3dc7f258e320f6d3420603505d
abdalrzag eisa <bmjfdrh@gmail.com>
2022-07-23 15:47:41 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use neoconf::Neoconf;
+use neoconf::{Neoconf, Options};
 
 fn main() {
-    let mut config = Neoconf::new("test.neoconf");
+    let mut config = Neoconf::new("test.neoconf", Options { auto_save: true });
 
     config.load();
 
     println!("testkey: {}", config.get(Some("testsection1"), "testkey").unwrap_or_default());
-
-    // config.remove(Some("testsection2"), "host");
-
+    
     println!("host: {}", config.get(Some("testsection2"), "host").unwrap_or_default());
 
     println!("port: {}", config.get(Some("testsection2"), "port").unwrap_or_default());
 
     println!("name: {}", config.get(None, "name").unwrap_or_default()); 
 
-    config.save().unwrap();
+    // config.save().unwrap();
 }
