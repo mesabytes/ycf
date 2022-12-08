0274c31907ecdad6db45d85dccbc8004354bf80a
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-08 19:36:15 +0300

    remove neoconf-syntax install script

--- install-syntax-highlighting.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install the syntax highlighting vscode extension
-
-VSCODE_EXTENSION_PATH=${HOME}/.vscode-oss/extensions
-
-cp -rv ./neoconf-syntax-highlighting ${VSCODE_EXTENSION_PATH}
