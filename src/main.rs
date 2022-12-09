25c52cbc07b2a736de94c76ede6918194b11731f
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-09 12:35:37 +0300

    clean main fn

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,3 @@
-use neoconf::{Neoconf, Options};
-
 fn main() {
-    let mut config = Neoconf::load("test.neoconf", Options::default());
 
-    config.load();
-
-    println!(
-        "testkey: {}",
-        config
-            .get(Some("testsection1"), "testkey")
-            .unwrap_or_default()
-    );
-
-    println!(
-        "host: {}",
-        config.get(Some("testsection2"), "host").unwrap_or_default()
-    );
-
-    println!(
-        "port: {}",
-        config.get(Some("testsection2"), "port").unwrap_or_default()
-    );
-
-    println!("name: {}", config.get(None, "name").unwrap_or_default());
-
-    // config.save().unwrap();
 }
