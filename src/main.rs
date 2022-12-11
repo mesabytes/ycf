85f0611bee32eb197e458e90a96038000907ad01
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-11 17:28:34 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use ycf::Ycf;
 
 fn main() {
-    let config = Ycf::load_from_file("test.ycf");
+    let mut config = Ycf::load_from_file("test.ycf");
 
+    let username = config.get("testsection1.user.username");
 
+    config.set("hello".into(), "world".into());
+
+    println!("{:?}", username);
+    println!("{:?}", config.get("hello").unwrap());
 }
