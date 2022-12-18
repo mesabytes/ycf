f9b25f703c3469f62853ecc4159e43ba7cf08226
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-18 23:13:24 +0300

    change ROOT_SECTION

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,5 @@
-pub const ROOT_SECTION: &str = "root";
+// SHA1 for "root"
+pub const ROOT_SECTION: &str = "dc76e9f0c0006e8f919e0c515c66dbba3982f785";
 
 pub const SECTION_PREFIX: &str = "@";
 pub const SECTION_END: &str = "}";
