e29dd98d3122e064b43252ed6b343f6d4ed1b85b
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-19 00:08:09 +0300

    add PREPROC_PREFIX const

--- src/constants.rs
+++ src/constants.rs
@@ -2,6 +2,7 @@
 pub const ROOT_SECTION: &str = "dc76e9f0c0006e8f919e0c515c66dbba3982f785";
 
 pub const SECTION_PREFIX: &str = "@";
+pub const PREPROC_PREFIX: &str = "#";
 pub const SECTION_END: &str = "}";
 pub const KEY_VALUE_SEP: &str = "=";
 pub const COMMENT_PREFIX: &str = ";;";
