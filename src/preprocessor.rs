8025dfb3528e1989a8d8d5d4ee62410bd03c997b
abdalrzag eisa <bmjfdrh@gmail.com>
2022-12-20 14:32:29 +0300

    impl import

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -13,23 +13,49 @@ pub fn preprocessor(file: Option<String>, input: String) -> String {
 
             let tokens: Vec<&str> = line.split_whitespace().collect();
 
-            let preprocessor = tokens[0];
-            if PREPROCS_LIST.contains(&preprocessor) {
-                match preprocessor {
+            let preprocessor_name = tokens[0];
+            if PREPROCS_LIST.contains(&preprocessor_name) {
+                match preprocessor_name {
                     "import" => {
                         let config_file: &String = match file {
                             Some(ref f) => f,
                             None => continue,
                         };
 
-                        let file_to_import = tokens.get(1).unwrap_or_else(|| {
-                            eprintln!("[ycf] Error: import path is missing!",);
-                            eprintln!("{line_number} | {line}", line_number = index + 1);
+                        let mut fpath = std::path::PathBuf::new();
 
+                        fpath.push(config_file);
+
+                        // remove config_file from path
+                        fpath.pop();
+
+                        let file_name = tokens.get(1).unwrap_or_else(|| {
+                            eprintln!("{line_number} | {line}", line_number = index + 1);
+                            eprintln!("[ycf] Error: import path is missing!");
                             exit(1);
                         });
 
-                        // TODO: check if file_to_import exists if not throw an error
+                        let file_to_import = fpath.join(file_name);
+
+                        if !file_to_import.exists() {
+                            eprintln!("{line_number} | {line}", line_number = index + 1);
+                            eprintln!("[ycf] Error: import path does not exists!");
+                            exit(1);
+                        }
+                        if file_to_import.is_dir() {
+                            eprintln!("{line_number} | {line}", line_number = index + 1);
+                            eprintln!("[ycf] Error: import path is a directory!");
+                            exit(1);
+                        }
+
+                        let file_to_import_content =
+                            std::fs::read_to_string(&file_to_import).expect("Failed to read file");
+
+                        // preprocess imported file content
+                        let content = preprocessor(file.clone(), file_to_import_content);
+
+                        new_input.push_str(&content);
+                        new_input.push('\n');
                     }
                     _ => {}
                 }
